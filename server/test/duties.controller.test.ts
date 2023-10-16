060ec8c830f552868f44be306e908f4b727669ab
Carlos Rivas <carlos.rivasp@alumnos.upm.es>
2023-10-16 12:26:06 +0200

    error handling in axios client

--- server/test/duties.controller.test.ts
+++ server/test/duties.controller.test.ts
@@ -1,7 +1,30 @@
 import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { Request, Response } from "express";
-import { Duty } from 'rest/controllers/duties.controller';
-import * as dutiesRequest from './duties.requests.data';
+import { Duty, DutyInput } from 'rest/controllers/duties.controller';
+
+export async function getDutiesRequest(axios: AxiosInstance): Promise<AxiosResponse<Duty[]>> {
+  return axios.get('/duty')
+}
+
+export async function createDutyRequest(axios: AxiosInstance, duty: DutyInput): Promise<AxiosResponse<string>> {
+  return axios.post('/duty', {
+    name: duty.name
+  })
+}
+
+export async function getDutyRequest(axios: AxiosInstance, id: string): Promise<AxiosResponse<Duty>> {
+  return axios.get(`/duty/${id}`)
+}
+
+export async function updateDutyRequest(axios: AxiosInstance, duty: Duty): Promise<AxiosResponse<string>> {
+  return axios.put(`/duty/${duty.id}`, {
+    name: duty.name,
+  });
+}
+
+export async function deleteDutyRequest(axios: AxiosInstance, id: string): Promise<AxiosResponse<string>> {
+    return axios.delete(`/duty/${id}`);
+  }
 
 describe('Tests for the API', () => {
 
@@ -9,13 +32,12 @@ describe('Tests for the API', () => {
 
   beforeAll(async () => {
     client = axios.create({
-      baseURL: 'localhost:4000',
-      timeout: 60000
+      baseURL: 'http://localhost:4000'
     })
   })
 
     it('Should return duty list', async () => {
-      const response = await dutiesRequest.getDutiesRequest(axios)
+      const response = await getDutiesRequest(client)
       expect(response.status).toEqual(200)
 
       const duties: Duty[] = response.data
@@ -23,27 +45,27 @@ describe('Tests for the API', () => {
     });
 
     it('Should create a new duty', async () => {
-      const response = await dutiesRequest.createDutyRequest(axios, { name: 'New duty' })  
+      const response = await createDutyRequest(client, { name: 'New duty' })  
       expect(response.status).toEqual(200)
       expect(response.data).toBeDefined()
     })
 
     it('Should get an specific duty', async () => {
-      const response = await dutiesRequest.getDutyRequest(axios, '35')
+      const response = await getDutyRequest(client, '115')
       expect(response.status).toEqual(200)
       const duty: Duty = response.data
       expect(duty).toBeDefined()
     })
 
     it('Should update an specific duty', async () => {
-      const response = await dutiesRequest.updateDutyRequest(axios, {id:'20', name:'Update Duty'})
+      const response = await updateDutyRequest(client, {id:'116', name:'Update Duty'})
       expect(response.status).toEqual(200)
       const id: string = response.data
       expect(id).toBeDefined()
     })
 
     it('Should delete an specific duty', async () => {
-      const response = await dutiesRequest.deleteDutyRequest(axios,'20')
+      const response = await deleteDutyRequest(client,'124')
       expect(response.status).toEqual(200)
     })
 
